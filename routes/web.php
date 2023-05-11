7788011d8a00024457abcc86ab6307379555b1f6
Shahbozbek <128174001+JumanovShahbozbek@users.noreply.github.com>
2023-05-11 17:34:23 +0500

    breeze

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\SiteController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -18,3 +18,15 @@ Route::get('/', [SiteController::class, 'welcome']);
 Route::get('/about', [SiteController::class, 'about']);
 Route::get('programs', [SiteController::class, 'programs']);
 Route::get('contact', [SiteController::class, 'contact']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+require __DIR__.'/auth.php';
